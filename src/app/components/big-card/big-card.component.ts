637055196a4bcc48430b7c337f0817c3bbb7cd7c
Gustavo Lima <gustavo.lima@sinqia.com.br>
2022-09-27 11:35:47 -0300

    content created

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -8,10 +8,9 @@ import { Component, Input, OnInit } from '@angular/core';
 
 export class BigCardComponent implements OnInit {
   @Input()photoCover:string =""
-
   @Input()cardTitle:string =""
-
   @Input()cardDescription:string =""
+  @Input()Id:string="0"
 
   constructor() { }
 
